059a303d43d59ec815f9d9422207511c84b3af13
Valeria Belan <valeria-belan@mail.ru>
2024-10-16 19:56:13 +0300

    lab_2

---
+++ Belan_30323.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Belan_30323.UI.ViewComponents
+{
+    public class CartViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Belan_30323.UI/Views/Shared/Components/Cart/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Belan_30323.UI.Views.Shared.Components.Cart
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
