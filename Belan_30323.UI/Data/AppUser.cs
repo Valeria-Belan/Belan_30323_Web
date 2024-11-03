d81986e80e96ffba452931d3f24c688ab7f1cfaf
Valeria Belan <valeria-belan@mail.ru>
2024-11-03 19:15:43 +0300

    lab_3

---
+++ Belan_30323.UI/Data/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Belan_30323.UI.Data
+{
+    public class AppUser: IdentityUser
+    {
+    }
+}
--- Belan_30323.UI/Data/ApplicationDbContext.cs
+++ Belan_30323.UI/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Belan_30323.UI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
